27279c5a0602837799eb39d034bbbf9ecae24e10
Saara Satokangas <saarasat@gmail.com>
2020-07-10 11:09:56 +0300

    Showing questionnaire-hints more clearly
    - The hints are connected with the related checkbox-option
    - If there is a common hint for the question that will
    be shown below the whole question. These common hints are the
    ones shown when a correct answer is NOT chosen.
    - For other question-types the hints are related to the question
    like before.
    - The html-element of hints are changed from list to bootstrap
    help-blocks. These could be then used elsewhere for form-feedback
    as well.

--- access/templatetags/access.py
+++ access/templatetags/access.py
@@ -13,3 +13,23 @@ def ajax_url(context):
         args=[context['course']['key'], context['exercise']['key']]
     ))
     return ""
+
+@register.filter
+def find_checkbox_hints(list_of_hints, value):
+    hints = []
+    if list_of_hints:
+        for hint in list_of_hints:
+            if hint.get('value') == value:
+                hints.append(hint.get('label'))
+
+    return hints
+
+@register.filter
+def find_common_hints(list_of_hints, checked):
+    hints = []
+    if list_of_hints:
+        for hint in list_of_hints:
+            if hint.get('not') and hint.get('value') not in checked and hint.get('label') not in hints:
+                hints.append(hint.get('label'))
+
+    return hints
